6f56b65ad380737b43ea854da7c9e591c249255c
Christian Marienfeld <post@zwiebelgasse.de>
2024-08-31 18:21:46 +0200

    wip

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,9 @@
 <script>
 
 //import Greet from "./components/Greet.vue";
-import {open} from '@tauri-apps/plugin-dialog';
 import {Store} from '@tauri-apps/plugin-store';
-import {BaseDirectory, readDir} from '@tauri-apps/plugin-fs';
-import { fetch } from '@tauri-apps/plugin-http';
+import {BaseDirectory, open, readDir} from '@tauri-apps/plugin-fs';
+import {upload} from '@tauri-apps/plugin-upload';
 
 const store = new Store('store.bin');
 
@@ -12,39 +11,51 @@ export default {
   components: {},
   data() {
     return {
+      page: "home",
+      interval: false,
+      intervalToggle: 0,
+
       folder: "",
-      folderPrefixTeacher: "lz_",
-      folderPrefixPupil: "aula_",
+      folderPrefixTeacher: "",
+      folderPrefixPupil: "",
       apiUrl: "https://www.schule-intern.de/rest.php/vplan/updater",
-      apiKey: ""
+      apiKey: "",
+      power: false
     };
   },
   created() {
     this.load();
+
   },
   methods: {
 
     async load() {
 
       this.folder = await store.get('folder');
-      console.log('val', this.folder)
-      if (this.folder) {
-        this.watchFolder();
-      }
+      this.power = await store.get('power');
+      this.folderPrefixTeacher = await store.get('folderPrefixTeacher');
+      this.folderPrefixPupil = await store.get('folderPrefixPupil');
+      this.apiUrl = await store.get('apiUrl');
+      this.apiKey = await store.get('apiKey');
+
+      this.watchFolder();
 
     },
     async handlerFolder() {
+
 
       const file = await open({
         multiple: false,
         directory: true,
       });
+      //console.log(file)
       if (file) {
         this.folder = file;
-        await store.set('folder', {value: this.folder.value});
+        await store.set('folder', file);
         await store.save();
-        this.watchFolder();
+
       }
+
 
     },
     getFolderNames (name) {
@@ -52,58 +63,178 @@ export default {
     },
     async watchFolder() {
 
-      if (this.folder && this.folder.value) {
-        console.log('watch-', this.folder.value)
+      //console.log('watch-0', this.power)
 
-        if (this.apiUrl) {
-          const response = await fetch(this.apiUrl, {
-            method: 'GET',
-          });
-          console.log(response)
-        }
+      if (this.power) {
+        if (this.folder && this.apiUrl && this.apiKey) {
 
 
-        readDir(this.folder.value,{baseDir: BaseDirectory.Home}).then((folders) => {
-
-          folders.forEach((folder) => {
-            if (folder.isDirectory) {
-
-              if ( this.getFolderNames().includes(folder.name) ) {
-
-                readDir(this.folder.value+'/'+folder.name,{baseDir: BaseDirectory.Home}).then((files) => {
-
-                  files.forEach((file) => {
-                    if (file.isFile) {
-                      console.log(folder.name,file.name)
+          //console.log('watch-1', this.folder)
+          //console.log('watch-2', this.apiUrl)
+          //console.log('watch-3', this.apiKey)
 
 
-                    }
-                  })
-                });
+          this.interval = setInterval(this.handlerInterval, 5000);
+
+          //console.log(this.interval)
 
 
+          /*
+          await watch(
+              this.folder.value,
+              (event) => {
+                console.log('app.log event', event);
+              },
+              {
+                baseDir: BaseDirectory.AppLog,
+                recursive: true,
               }
+          );
+          */
+
+
+        } else {
+          this.handlerPage('settings');
+        }
+      }
+
+    },
+
+    async sendData(filepath) {
+
+      console.log(filepath)
+      this.intervalToggle++;
+
+      const res = await upload(
+          this.apiUrl,
+          filepath,
+          ({progress, total}) => {
+            //console.log(`Uploaded ${progress} of ${total} bytes`)
+          }, // a callback that will be called with the upload progress
+          {
+            'Content-Type': 'text/plain',
+            "auth-app": this.apiKey,
+            'auth-session': "false"
+          } // optional headers to send with the request
+      );
+
+      console.log(res)
+
+      if (res) {
+        if (this.intervalToggle > 0) {
+          this.intervalToggle--;
+        }
+      }
+
+
+
+
+      /*
+      var formdata = new FormData();
+      formdata.append("html", data);
+      formdata.append("file", 1111);
+
+      fetch(this.apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          "auth-app": this.apiKey,
+          'auth-session': false
+        }
+      })
+      .then((response) => {
+
+        console.log(response)
+        if (response.status == 200) {
+          const json = response.text();
+          console.log(json);
+        } else {
+          console.log('Error', response)
+        }
+        this.intervalToggle--;
+
+      })
+      */
+
+
+    },
+
+    handlerInterval() {
+
+      //console.log('intrval');
+
+
+      readDir(this.folder, {baseDir: BaseDirectory.Home}).then((folders) => {
+
+
+        folders.forEach((folder) => {
+          if (folder.isDirectory) {
+
+            if (this.getFolderNames().includes(folder.name)) {
+
+              readDir(this.folder + '/' + folder.name, {baseDir: BaseDirectory.Home}).then((files) => {
+
+                files.forEach(async (file) => {
+                  if (file.isFile) {
+
+                    //console.log(this.folder+'/'+folder.name+'/'+file.name)
+                    /*
+                        const contents = await open(this.folder + '/' + folder.name + '/' + file.name, {baseDir: BaseDirectory.Home});
+                        const buf = new Uint8Array(50000);
+                        const numberOfBytesRead = await contents.read(buf); // 11 bytes
+                        const text = new TextDecoder().decode(buf);  // "hello world"
+                        await contents.close();
+                    */
+
+
+                    this.sendData(this.folder + '/' + folder.name + '/' + file.name);
+
+                  }
+                })
+              });
+
+
             }
-          })
-        });
+          }
+        })
+      });
+      //this.intervalToggle = false;
 
 
-        /*
-        await watch(
-            this.folder.value,
-            (event) => {
-              console.log('app.log event', event);
-            },
-            {
-              baseDir: BaseDirectory.AppLog,
-              recursive: true,
-            }
-        );
-        */
+    },
 
+    handlerSave() {
+
+      store.set('folderPrefixTeacher', this.folderPrefixTeacher);
+      store.set('folderPrefixPupil', this.folderPrefixPupil);
+      store.set('apiUrl', this.apiUrl);
+      store.set('apiKey', this.apiKey);
+      store.save();
+
+      this.handlerPage();
+    },
+
+    handlerPower() {
+      this.power = !this.power;
+      store.set('power', this.power);
+      store.save();
+
+      if (this.power) {
+        this.load();
+      } else {
+        clearInterval(this.interval);
 
       }
 
+    },
+
+    handlerPage(page) {
+      if (!page) {
+        page = 'home';
+      }
+      this.page = page;
     }
   },
 };
@@ -113,9 +244,16 @@ export default {
 
 <template>
   <div class="container">
-    <div class="container-home">
+    <div class="header">
+      <button @click="handlerPage('home')">Home</button>
+      <button @click="handlerPage('settings')">Settings</button>
     </div>
-    <div class="container-settings">
+    <div class="container-home" v-if="page == 'home'">
+      <div class="button" @click="handlerPower">Button {{ this.power }}</div>
+      {{ intervalToggle }}
+    </div>
+
+    <div class="container-settings" v-if="page == 'settings'">
       <ul>
         <li>
           <label>Folder</label>
@@ -137,6 +275,9 @@ export default {
         <li>
           <label>apiKey:</label>
           <input v-model="apiKey">
+        </li>
+        <li>
+          <button @click="handlerSave">Speichern</button>
         </li>
       </ul>
     </div>
